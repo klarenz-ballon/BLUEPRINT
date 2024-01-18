e454e85ca4a9599a8dd158f9027ddf2ba7d11328
klarenz-ballon <knballon@up.edu.ph>
2024-01-19 07:53:33 +0800

    edit on heading style only

--- pages/updatealum.py
+++ pages/updatealum.py
@@ -6,7 +6,7 @@ layout=html.Div([
     html.Div([
         cm.navigationpanel,
         html.Div([
-    html.H2("Update Alumni")
+    html.H2("Update Alumni",style={"font-family": "Arial", "color": "#273250"})
 ],className="body")
     ],className='flex container')
 ])
