8f1c4a7dffdd5479f8d38bb562be62b587d52e64
anagramiccoder <jdvidad31@gmail.com>
2024-01-19 08:15:32 +0800

    reaffiliate added more info input

--- pages/reaffiliate.py
+++ pages/reaffiliate.py
@@ -10,7 +10,14 @@ head_style = {
     "font-family": "Arial",
     "color": "#273250",
         }
-
+comms=[
+        "Academic Affairs Committee",
+        "External Affairs Committee",
+        "Finance Committee",
+        "Internal Affairs Committee",
+        "Membership and Recruitment Committee",
+        "Publications and Records Committee"
+]
 
 
 layout=html.Div([
@@ -47,6 +54,17 @@ layout=html.Div([
             html.Div([html.H3("Present Address:"),dcc.Input(type='text', id='reaf-presadd', required=True),],className='flex add'),
             html.Div([html.H3("Permanent Address:"),dcc.Input(type='text', id='reaf-permadd', required=True)],className='flex add'),
             ],className='address'),
+        html.Div([
+            html.Div([html.H3("Degree Program:"),dcc.Input(type='text',id='reaf-degree'),
+                      html.H3("Year Standing:"),dcc.Dropdown(id='year-stand',options=[1,2,3,4,5,6,7,8], value=1,searchable=False,clearable=False),
+                      html.H3("Membership type:"),dcc.Dropdown(id='reaf-type',options=["Regular","Non-Regular","Honorary","Probationary"],value='Regular',searchable=False,clearable=False,style={'width':'100%'}),
+                      html.H3("App Batch:"),dcc.Input(id='reaf-batch',type='text'),
+                      html.H3("GWA:"),dcc.Input(type='text', id ='reaf-gwa')
+                    ],className='flex edit name'),
+                    html.H3("Committee Preferences (Select from first priority to last priority)"),
+           html.Div([dcc.Dropdown(id='ranking-dropdown', options=[{'label': choice, 'value': choice} for choice in comms], multi=True,style={'width':'100%'})],className='flex edit others') 
+        ]),
+        html.Div([html.H3("Reaffilation Fee"),dcc.Dropdown(id='reaff-fee',options=["pay as I submit","pay at a later Date"],value='pay as I submit',searchable=False,clearable=False,style={'width':200})],className='flex'),
         html.Div([html.Button("Submit Form",id='reaff-btn',n_clicks=0, className="choice")],className='flex last')
     ],className='edit'),
 ],className="body")
@@ -70,16 +88,26 @@ layout=html.Div([
     Input('reaf-vid','value'),
     Input('reaf-presadd','value'),
     Input('reaf-permadd','value'),
+    Input('reaf-degree','value'),
+    Input('year-stand','value'),
+    Input('reaf-type','value'),
+    Input('reaf-batch','value'),
+    Input('reaf-gwa','value'),
+    Input('ranking-dropdown','value'),
+    Input('reaff-fee','value'),
 ],
 )
-def reaff_member(btn,fname,mname,lname,sfx,bday,cn,ecn,email,vid,presadd,permadd):
+def reaff_member(btn,fname,mname,lname,sfx,bday,cn,ecn,email,vid,presadd,permadd,deg,ys,rtype,btc,gwa,ranks,fee):
     if btn>0 :
         #check if there is existing data
-        print("Checking")
-        sql="SELECT is_new FROM affiliation WHERE valid_id='"+vid+"'"
+        commpref=[None,None,None,None,None,None]
+        print("checking")
+        sql="SELECT valid_id FROM person WHERE valid_id='"+vid+"'"
         cols=['new']
         df=db.querydatafromdatabase(sql,[],cols)
+        print("checked")
         if df.shape[0]:
+            print("existing")
             #there exist a data
             #update existing data
             sql="""
@@ -99,13 +127,34 @@ def reaff_member(btn,fname,mname,lname,sfx,bday,cn,ecn,email,vid,presadd,permadd
             """
             values=[fname,mname,lname,sfx,bday,cn,ecn,email,presadd,permadd,vid]
             db.modifydatabase(sql,values)
-            #update is_new to false
-            sql="UPDATE affiliation SET is_new=False WHERE valid_id=%s"
-            values=[vid]
+            #update affiliation
+            print("updating affiliation")
+            for i,j in enumerate(ranks):
+                commpref[i]=j#update rankings of preference, None in case not selected
+            sql="""
+                UPDATE affiliation 
+                SET 
+                is_new=False,
+                degree_program=%s,
+                membership_type=%s,
+                year_standing=%s,
+                app_batch=%s,
+                comm_firstchoice=%s,
+                comm_secondchoice=%s,
+                comm_thirdchoice=%s,
+                comm_fourthchoice=%s,
+                comm_fifthchoice=%s,
+                comm_sixthchoice=%s,
+                gwa=%s,
+                reaff_fee=%s
+                WHERE valid_id=%s"""
+            values=[deg,rtype,ys,btc,commpref[0],commpref[1],commpref[2],commpref[3],commpref[4],commpref[5],gwa,fee,vid]
             db.modifydatabase(sql,values)
+            print("done updating affilation")
             return 'shown modal', 'shown modal-background',dash.no_update
         else:  #there is no existing data
             #add new person sincec foreign key first
+            print("new")
             sql="""
             INSERT INTO person(valid_id, first_name,middle_name,last_name,suffix,birthdate,contact_number, emergency_contact_number,email,present_address, permanent_address)
             VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
@@ -118,9 +167,10 @@ def reaff_member(btn,fname,mname,lname,sfx,bday,cn,ecn,email,vid,presadd,permadd
             db.modifydatabase(sql,values)
             #add new row in table affiliation, sql can be edited if more info is needed
             sql="""
-            INSERT INTO affiliation(valid_id,reaff_fee,is_new)
-            VALUES (%s,'pay as I submit',True)
+            INSERT INTO affiliation(valid_id,is_new,degree_program,membership_type,year_standing,app_batch,comm_firstchoice,comm_secondchoice,comm_thirdchoice,comm_fourthchoice,comm_fifthchoice,comm_sixthchoice,gwa,reaff_fee)
+            VALUES (%s,True,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
             """
+            values=[vid,deg,rtype,ys,btc,commpref[0],commpref[1],commpref[2],commpref[3],commpref[4],commpref[5],gwa,fee]
             db.modifydatabase(sql,values)
             return 'shown modal', 'shown modal-background','New member Successfully added'
     raise PreventUpdate
